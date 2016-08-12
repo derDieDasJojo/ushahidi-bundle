3245f39bceeb6c72fe29f700f926b5caa7725dfc
Jojo <git config --global user.name Your>
2016-08-12 13:52:07 +0200

    bugfix: page redirect. switched to nginx

--- client/start.sh
+++ client/start.sh
@@ -3,4 +3,5 @@ set -o pipefail
 
 grep -Rl '{{backend_url}}' /var/www | xargs sed -i "s#{{backend_url}}#$BACKEND_URL#g"
 
-http-server /var/www -p 80
+#http-server /var/www -p 80
+nginx -g 'daemon off;'
